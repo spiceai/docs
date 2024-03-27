aed71da6b0529bfed61f68b11b35eb454118ca7e
jeadie <jack@spice.ai>
2024-03-28 08:14:20 +1100

    code formatting for bash

--- spiceaidocs/docusaurus.config.ts
+++ spiceaidocs/docusaurus.config.ts
@@ -137,6 +137,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['bash'],
     },
     algolia: {
       appId: '0SP8I8JTL8',
