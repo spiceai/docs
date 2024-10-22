3d64757b88f8ab598ee9454a59be99137cba643d
Sevenannn <qianqliu@uw.edu>
2024-10-21 19:51:28 -0700

    Update doc banner to v0.19.2

--- spiceaidocs/docusaurus.config.ts
+++ spiceaidocs/docusaurus.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
     // image: 'img/docusaurus-social-card.jpg',
     announcementBar: {
       content:
-        '<a href="https://github.com/spiceai/spiceai/releases/tag/v0.19.1-beta">Spice.ai OSS v0.19.1-beta</a> is now available! 🚀',
+        '<a href="https://github.com/spiceai/spiceai/releases/tag/v0.19.2-beta">Spice.ai OSS v0.19.2-beta</a> is now available! 🚀',
       backgroundColor: 'var(--announcement-bar-bg)',
       textColor: 'var(--announcement-bar-text)',
       isCloseable: true,
