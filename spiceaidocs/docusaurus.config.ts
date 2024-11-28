95d0dc1933879f0e269e406d0596289573447d9a
Sevenannn <qianqliu@uw.edu>
2024-11-27 16:54:45 -0800

    Update version to v1.0-rc.1

--- spiceaidocs/docusaurus.config.ts
+++ spiceaidocs/docusaurus.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
     // image: 'img/docusaurus-social-card.jpg',
     announcementBar: {
       content:
-        '<a href="https://github.com/spiceai/spiceai/releases/tag/v0.20.0-beta">Spice.ai OSS v0.20.0-beta</a> is now available! 🚀',
+        '<a href="https://github.com/spiceai/spiceai/releases/tag/v1.0-rc.1">Spice.ai OSS v1.0-rc.1</a> is now available! 🚀',
       backgroundColor: 'var(--announcement-bar-bg)',
       textColor: 'var(--announcement-bar-text)',
       isCloseable: true,
