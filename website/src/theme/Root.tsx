78d902f32ee908440fc892f0f4302b1666ee6324
Evgenii Khramkov <evgenii@spice.ai>
2025-01-14 22:24:26 +0900

    Remove scarf pixel when running locally in dev

--- website/src/theme/Root.tsx
+++ website/src/theme/Root.tsx
@@ -1,16 +1,24 @@
 import React, { type PropsWithChildren } from "react";
 
+function ScarfPixel() {
+  if (process.env.NODE_ENV === 'development') {
+    return null;
+  }
+
+  return <img
+    referrerPolicy="no-referrer-when-downgrade"
+    src="https://static.scarf.sh/a.png?x-pxid=d13c8a61-d145-479a-92dc-6a231eddbb3e"
+    style={{
+      position: "absolute",
+    }}
+  />;
+}
+
 export default function Root({ children }: PropsWithChildren) {
   return (
     <>
       {children}
-      <img
-        referrerPolicy="no-referrer-when-downgrade"
-        src="https://static.scarf.sh/a.png?x-pxid=d13c8a61-d145-479a-92dc-6a231eddbb3e"
-        style={{
-          position: "absolute",
-        }}
-      />
+      <ScarfPixel />
     </>
   );
 }
