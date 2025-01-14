cbb8fcf21154eb94f488b7227f85e70b2f50eca4
Evgenii Khramkov <evgenii@spice.ai>
2025-01-14 20:31:24 +0900

    Switch base url to spiceai.org (#751)

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.spiceai.org',
+  url: 'https://spiceai.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
