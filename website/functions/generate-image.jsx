8fbce4535904dde5b73585ebc432a27eb0d42ec2
Evgenii Khramkov <evgenii@spice.ai>
2025-01-22 12:10:34 +0900

    try using jsx

--- website/functions/generate-image.jsx
+++ website/functions/generate-image.jsx
@@ -1,40 +1,92 @@
 import { ImageResponse } from 'workers-og'
 
-export async function onRequest(context) {
-  try {
-    const url = new URL(context.request.url);
-
-    const title = url.searchParams.get('title') || 'Default Title'
-    const html = `
-      <div style="height: 100%; width: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: black; font-family: Manrope, Arial, sans-serif; background-repeat: no-repeat; background-size: cover;">
-      <div style="display: flex; flex-direction: column; height: 100%; width: 100%; background-image: url('${url.protocol}//${url.host}/img/bg-articles.png'); background-size: 100% 100%;">
-        <div style="display: flex; flex-direction: column; width: 100%; height: 100%; align-items: center; justify-content: center;">
-        <div style="display: flex; width: 100%;">
-          <div style="display: flex; flex-direction: column; width: 100%; justify-content: space-between; padding: 32px;">
-          <h2 style="font-family: Arial, sans-serif; font-size: 48px; font-weight: bold; letter-spacing: -0.05em; color: white; text-align: left; margin-left: 80px; width: 384px; margin-top: 40px;">
-            ${title}
-          </h2>
+function template({ title }) {
+  return (
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'black',
+        fontFamily: 'Manrope, Arial, sans-serif',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          width: '100%',
+          backgroundImage: `url('${url.protocol}//${url.host}/img/bg-articles.png')`,
+          backgroundSize: '100% 100%'
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            height: '100%',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <div style={{ display: 'flex', width: '100%' }}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%',
+                justifyContent: 'space-between',
+                padding: '32px'
+              }}
+            >
+              <h2
+                style={{
+                  fontFamily: 'Open Sans, sans-serif',
+                  fontSize: '48px',
+                  fontWeight: 'bold',
+                  letterSpacing: '-0.05em',
+                  color: 'white',
+                  textAlign: 'left',
+                  marginLeft: '80px',
+                  width: '384px',
+                  marginTop: '40px'
+                }}
+              >
+                {title}
+              </h2>
+            </div>
           </div>
         </div>
-        </div>
       </div>
-      </div>
-    `
+    </div>
+  )
+}
 
-    return new ImageResponse(
-      html,
-      {
-        width: 800,
-        height: 418,
-        fonts: [
-          {
-            name: 'Open Sans',
-            data: await loadGoogleFont('Open Sans', title),
-            style: 'normal',
-          },
-        ],
-      }
-    )
+export async function onRequest(context) {
+  try {
+    const url = new URL(context.request.url)
+
+    const title = url.searchParams.get('title') || 'Default Title'
+    const html = template({ title })
+
+    return new ImageResponse(html, {
+      width: 800,
+      height: 418,
+      fonts: [
+        {
+          name: 'Open Sans',
+          data: await loadGoogleFont('Open Sans', title),
+          style: 'normal'
+        }
+      ]
+    })
   } catch (error) {
     console.error('Error generating image:', error)
     return new Response('Error generating image', { status: 500 })
@@ -45,13 +97,13 @@ async function loadGoogleFont(font, text) {
   const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(text)}`
   const css = await (await fetch(url)).text()
   const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)
- 
+
   if (resource) {
     const response = await fetch(resource[1])
     if (response.status == 200) {
       return await response.arrayBuffer()
     }
   }
- 
+
   throw new Error('failed to load font data')
 }
