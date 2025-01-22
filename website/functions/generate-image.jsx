254d419f6ea3a4c78397f9cc2fea82b001c4ccd7
Evgenii Khramkov <evgenii@spice.ai>
2025-01-22 12:28:04 +0900

    fix

--- website/functions/generate-image.jsx
+++ website/functions/generate-image.jsx
@@ -6,12 +6,12 @@ export async function onRequest(context) {
 
     const title = url.searchParams.get('title') || 'Default Title'
     const html = `
-      <div style="height: 100%; width: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: black; font-family: \"Open Sans\", sans-serif; background-repeat: no-repeat; background-size: cover;">
+      <div style="height: 100%; width: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: black; font-family: 'Open Sans', sans-serif; background-repeat: no-repeat; background-size: cover;">
       <div style="display: flex; flex-direction: column; height: 100%; width: 100%; background-image: url('${url.protocol}//${url.host}/img/bg-articles.png'); background-size: 100% 100%;">
         <div style="display: flex; flex-direction: column; width: 100%; height: 100%; align-items: center; justify-content: center;">
         <div style="display: flex; width: 100%;">
           <div style="display: flex; flex-direction: column; width: 100%; justify-content: space-between; padding: 32px;">
-          <h2 style="font-family: \"Open Sans\", sans-serif; font-size: 48px; font-weight: bold; letter-spacing: -0.05em; color: white; text-align: left; margin-left: 80px; width: 384px; margin-top: 40px;">
+          <h2 style="font-family: 'Open Sans', sans-serif; font-size: 48px; font-weight: bold; letter-spacing: -0.05em; color: white; text-align: left; margin-left: 70px; width: 384px; margin-top: 40px;">
             ${title}
           </h2>
           </div>
