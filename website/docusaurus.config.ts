2cf334866cd2b5d19738138c675ff7f84041aa7e
karifabri <137208546+karifabri@users.noreply.github.com>
2025-04-08 15:57:21 -0400

    banner update

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -92,7 +92,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/og/spiceai.png',
     announcementBar: {
-      content: '<a href="/blog/releases/v1.1.0">Spice.ai OSS v1.1.0</a> is now available! 🚀',
+      content: '<a href="/blog/releases/v1.1.0">Spice.ai OSS v1.1.1</a> is now available! 🚀',
       backgroundColor: 'var(--announcement-bar-bg)',
       textColor: 'var(--announcement-bar-text)',
       isCloseable: true
