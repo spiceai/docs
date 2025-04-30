d1655d8bba9057f2e5208c00d468ee088d61ad32
Luke Kim <80174+lukekim@users.noreply.github.com>
2025-04-29 17:12:58 -0700

    Remove annoying OGP image

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -89,8 +89,6 @@ const config: Config = {
   ],
   themes: ['docusaurus-theme-openapi-docs'],
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/og/spiceai.png',
     announcementBar: {
       content: '<a href="/blog/releases/v1.2.0">Spice.ai OSS v1.2.0</a> is now available! 🚀',
       backgroundColor: 'var(--announcement-bar-bg)',
@@ -119,6 +117,29 @@ const config: Config = {
         },
         { to: 'blog', label: 'Blog', position: 'left' },
         { to: 'cookbook', label: 'Cookbook', position: 'left' },
+        {
+          type: 'dropdown',
+          label: 'Community',
+          position: 'left',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/kZnTfneP5u'
+            },
+            {
+              label: 'X',
+              href: 'https://x.com/spice_ai'
+            },
+            {
+              label: 'Reddit',
+              href: 'https://reddit.com/r/spiceai'
+            },
+            {
+              label: 'YouTube',
+              href: 'https://www.youtube.com/@spiceai'
+            }
+          ]
+        },
         {
           label: 'X',
           href: 'https://x.com/spice_ai',
