607fe53f8c68aac7e55f2a1e3ee59f3f7186dfc7
wyattwenzel <109381471+wyattwenzel@users.noreply.github.com>
2025-10-07 11:33:33 -0700

    Updated docs banner wtih v1.8.0

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -99,7 +99,7 @@ const config: Config = {
   themes: ['docusaurus-theme-openapi-docs'],
   themeConfig: {
     announcementBar: {
-      content: '<a href="/blog/releases/v1.6.1">Spice.ai OSS v1.6.1</a> is now available! 🚀',
+      content: '<a href="/blog/releases/v1.8.0">Spice.ai OSS v1.8.0</a> is now available! 🚀',
       backgroundColor: 'var(--announcement-bar-bg)',
       textColor: 'var(--announcement-bar-text)',
       isCloseable: true
