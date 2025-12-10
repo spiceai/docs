c32fa7bd0113a1806fe8f6a97c7e25adbfa3d315
Wyatt Wenzel <109381471+wyattwenzel@users.noreply.github.com>
2025-12-10 07:58:46 -0800

    Updated blog banner URL

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -100,7 +100,7 @@ const config: Config = {
   themeConfig: {
     announcementBar: {
       content:
-        '<a href="/blog/releases/v1.10.0">Spice.ai OSS v1.10.0</a> is now available! ⚡',
+        '<a href="/blog/releases/v1.10-0">Spice.ai OSS v1.10.0</a> is now available! ⚡',
       backgroundColor: 'var(--announcement-bar-bg)',
       textColor: 'var(--announcement-bar-text)',
       isCloseable: true
