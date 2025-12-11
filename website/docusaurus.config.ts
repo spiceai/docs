074e63b408d1676dd726d053e5340bb61f7fb6ec
Luke Kim <80174+lukekim@users.noreply.github.com>
2025-12-11 11:45:24 +0900

    Fix URL

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -99,7 +99,7 @@ const config: Config = {
   themes: ['docusaurus-theme-openapi-docs'],
   themeConfig: {
     announcementBar: {
-      content: '<a href="/blog/releases/v1.10-0">Spice.ai OSS v1.10.0</a> is now available! ⚡',
+      content: '<a href="/blog/releases/v1.10.0">Spice.ai OSS v1.10.0</a> is now available! ⚡',
       backgroundColor: 'var(--announcement-bar-bg)',
       textColor: 'var(--announcement-bar-text)',
       isCloseable: true
