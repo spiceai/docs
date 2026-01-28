c7a5ce513bed06d2a5ca10d64be4aa0a40d37458
Wyatt Wenzel <109381471+wyattwenzel@users.noreply.github.com>
2026-01-28 13:25:02 -0800

    Release Post for v1.11.0 (#1346)

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -208,7 +208,7 @@ const config: Config = {
       { name: 'twitter:creator', content: '@spice_ai' }
     ],
     announcementBar: {
-      content: '<a href="/releases/v1.10.4">Spice.ai OSS v1.10.4</a> is now available! ⚡',
+      content: '<a href="/releases/v1.11.0">Spice.ai OSS v1.11.0</a> is now available! ⚡',
       backgroundColor: 'var(--announcement-bar-bg)',
       textColor: 'var(--announcement-bar-text)',
       isCloseable: true
