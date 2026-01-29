e1ec429b5b041c0cf0bd3a65bf357378d273c281
Luke Kim <80174+lukekim@users.noreply.github.com>
2026-01-28 17:40:38 -0800

    Fix deprecation: move onBrokenMarkdownLinks to markdown.hooks

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -116,12 +116,14 @@ const config: Config = {
   organizationName: 'spiceai', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
 
-  onBrokenAnchors: 'warn',
-  // Use 'warn' instead of 'throw' because versioned docs from older branches
-  // may contain broken links that cannot be fixed retroactively.
-  // The build will still warn about broken links so they can be reviewed.
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenAnchors: 'throw',
+  onBrokenLinks: 'throw',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'throw'
+    }
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
