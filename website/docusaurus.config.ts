8c934b89679eed3ec005223d47443ffa07e5d3b3
Wyatt Wenzel <109381471+wyattwenzel@users.noreply.github.com>
2026-04-10 16:21:11 -0700

    Change banner

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -214,7 +214,7 @@ const config: Config = {
       { name: 'twitter:creator', content: '@spice_ai' }
     ],
     announcementBar: {
-      content: '<a href="/releases/v1.11.5">Spice.ai OSS v1.11.5</a> is now available! 🛠️',
+      content: '<a href="/releases/v2.0-rc.2">Spice.ai OSS v2.0-rc.2</a> is now available! 🛠️',
       backgroundColor: 'var(--announcement-bar-bg)',
       textColor: 'var(--announcement-bar-text)',
       isCloseable: true
